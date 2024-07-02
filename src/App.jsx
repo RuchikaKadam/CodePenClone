f85efe734e4c5ae75e4ffcefe7dd7a0372e0e624
RuchikaKadam <kadamruchikafullstack.com>
2024-07-02 13:53:18 +0530

    save data to firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Routes, Route, Navigate, useNavigate} from 'react-router-dom';
 import { Home } from './components';
-import { auth } from './config/firebase.config';
+import { auth, db } from './config/firebase.config';
+import { doc, setDoc } from 'firebase/firestore';
 
 const App = () => {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userCred) => {
       if(userCred){
-        console.log(userCred?.providerData[0].email);
+        console.log(userCred?.providerData[0]);
+        setDoc(doc(db, "users", userCred?.uid), userCred?.providerData[0]).then(() => {
+          //dispatch the action to the redux store
+        })
       } else {
         navigate("/home/auth", {replace: true })
       }
     })
   
+    //clean up the listenerEvent to avoid constant-re-rendering of this code
+     return ()=> unsubscribe();
   }, [])
   
   return (
+    <>
+    {isLoading ? (
+      <div className='w-screen h-screen flex items-center justify-center overflow-hidden' >
+ 
+    </div>
+    ) : (
     <div className='w-screen h-screen flex items-start justify-start overflow-hidden'>
         <Routes>
             <Route path="/home/*" element={<Home />} />
@@ -26,7 +40,9 @@ use Navigate from react-router-dom to re-direct the user.
  */}
             <Route path="*" element={<Navigate to ={"/home"}/>} />
         </Routes>
-    </div>
+    </div>)
+    }
+    </>
   );
 }
 
